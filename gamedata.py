d8f892937a0cb8748b79bbcf5d0fc79874fc379a
msloehr <msloehr@mtu.edu>
2020-10-25 16:51:01 -0400

    Committing my script that pulled game info from steam, the entire applist of steam, and all games on steam with their relevant info in both csv and json form.

---
+++ gamedata.py
@@ -0,0 +1,29 @@
+# By Matthew Loehr
+# Script for making csv file with all relevant information that we need from steam games
+import steamfront
+import csv
+import time
+import simplejson
+
+# opens the game list csv file we have
+with open("./steamgamelist.csv") as csvfile:
+    csv_reader = csv.reader(csvfile,delimiter=',')
+
+    #creates new csv that will store all relevant game data to us
+    with open("relevantgamedata.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["name","appid", "categories","genres","metacritic"])
+
+        client = steamfront.Client()
+        for i in range(102004):
+            next(csv_reader)
+        for row in csv_reader:
+            try:
+                if(str(client.getApp(appid=row[0]).type) == "game"):
+                    writer.writerow([row[1],row[0],str(client.getApp(appid=row[0]).categories),str(client.getApp(appid=row[0]).genres),str(client.getApp(appid=row[0]).metacritic)])
+                    time.sleep(1)
+                else:
+                    pass
+            except (steamfront.errors.AppNotFound, TypeError, simplejson.errors.JSONDecodeError):
+                pass
+
