574b70cc5b87e5a00175e097c8871f4d150bf11c
Gary Watson <glwatson@mtu.edu>
2020-10-30 16:31:14 -0400

    app displays games from database

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,11 @@
+#cs3141
+#teamc
+
 from django.contrib import admin
 
+from .models import Game, Categories, Genre
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(Categories)
+admin.site.register(Genre)
+
--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class  Game(models.Model):
+    name = models.CharField(max_length=100)
+    appid = models.IntegerField(primary_key=True)
+    score = models.IntegerField() #-1 = metacritic
+
+    def __str__(self):
+        return f"{self.name} - {self.appid} - {self.score}"
+
+class Categories(models.Model):
+    name = models.ForeignKey(Game, on_delete=models.CASCADE)
+    categories = models.CharField(max_length=100)
+
+class Genre(models.Model):
+    name = models.ForeignKey(Game, on_delete=models.CASCADE)
+    genre = models.CharField(max_length=100)
+
+    #Return a formatted tuple 
+    def __str__(self):
+        return f"{self.name}, {self.appid}, {self.categories}, {self.genre}, {self.score}"
+
