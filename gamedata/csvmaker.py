e7f2d09502f8fc7fb1044ada3cb9cc1f0ae0f0ee
msloehr <70709052+msloehr@users.noreply.github.com>
2020-11-06 10:46:52 -0500

    Script for splitting up the game csv and additional user profile data

---
+++ gamedata/csvmaker.py
@@ -0,0 +1,55 @@
+# Matthew Loehr
+# Python script for splitting up the relevantgamedata csv into useable csv's for our database according to Gary's specification
+import csv
+
+with open("./relevantgamedata.csv") as csvfile:
+    csv_reader = csv.reader(csvfile,delimiter=',')
+    
+    # Reformats the game title, appid, and metacritic scores
+    with open("reformattedgames.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["name","appid", "score"])
+
+        for i in range(1):
+            next(csv_reader)
+        for row in csv_reader:
+            score = row[4]
+            if score == "'metacritic'":
+                score = 0
+            else:
+                score = int(score[10:12])
+            writer.writerow([row[0],row[1],score])
+        f.close()
+
+    # reformats the categories each game
+    csvfile.seek(0)
+    for i in range(1):
+        next(csv_reader)
+    with open("reformattedcategories.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["appid","categories"])
+
+        for row in csv_reader:
+            appid = row[1]
+            categories = row[2].split("', '")
+            categories[0]=categories[0][2:]
+            categories[len(categories)-1]=categories[len(categories)-1][0:len(categories[len(categories)-1])-2]
+            for i in range(len(categories)):
+                writer.writerow([appid,categories[i]])
+        f.close()
+
+    # reformats the generes of each game
+    csvfile.seek(0)
+    for i in range(1):
+        next(csv_reader)
+    with open("reformattedgenres.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["appid","genres"])
+
+        for row in csv_reader:
+            appid = row[1]
+            genres = row[3].split("', '")
+            genres[0]=genres[0][2:]
+            genres[len(genres)-1]=genres[len(genres)-1][0:len(genres[len(genres)-1])-2]
+            for i in range(len(genres)):
+                writer.writerow([appid,genres[i]])
--- userprofile/userProfile.py
+++ userprofile/userProfile.py
@@ -6,14 +6,16 @@ f = open("../steamapikey.txt")
 apikey = f.readline()
 f.close()
 
-#done by Matthew Loehr
+#Returns a user object from the steamfront package
 def getUserProfile(id):
     client = steamfront.Client(apikey)
     return client.getUser(id64 = id)
 
+#returns the applications that are tied with a profile
 def getGameList(profile):
     return profile.apps
 
+#returns a dictionary with all the relevant game information we are storing 
 def getGameInfo(list):
     gameDict = {}
     for i in range(len(list)):
@@ -25,9 +27,11 @@ def getGameInfo(list):
             pass
     return gameDict
 
+#returns the steam profile name
 def getSteamName(profile):
     return profile.name  
 
+#creates a new entry in the profile csv when logging in with a username and steam ID
 def buildProfile(user, id):
     with open("UserProfiles.csv", "a", newline="") as f:
         csvwriter = csv.writer(f)
@@ -37,6 +41,7 @@ def buildProfile(user, id):
         csvwriter.writerow([user ,id, getSteamName(profile),games,len(games)])
 
 #done by Adam Johnson modified by Matthew Loehr
+#logs into mysteamlist when given the username and steam id
 def loginFcn():
     client = steamfront.Client(apikey)
     name = input("Enter mySteamList username: ")
